47c91b04ddd4f4ee951e44a989498b8f1b596f75
Sauzan Faza <sauzanfaza@gmail.com>
2025-09-19 10:30:16 +0700

    adding icons into every link in navbar, create logout button(only style), create profile pic

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,47 @@
+import { LuLayoutDashboard } from "react-icons/lu";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { BsStack } from "react-icons/bs";
+import { IoMdLogOut } from "react-icons/io";
+
 export default function Sidebar() {
     return (
-        <aside className="w-64 h-screen bg-gray-800 text-white p-4">
-        <h1 className="text-xl font-bold mb-6">Admin Dashboard</h1>
-        <nav className="space-y-4">
-            <a href="#" className="block hover:text-gray-300">Dashboard</a>
-            <a href="#" className="block hover:text-gray-300">Products</a>
-            <a href="#" className="block hover:text-gray-300">Users</a>
-            <a href="#" className="block hover:text-gray-300">Orders</a>
-        </nav>
+        <aside className="w-40 h-screen bg-gray-800 text-white p-4 flex flex-col">
+            {/* Profile Section */}
+            <div className="grid place-items-center mb-2 p-4">
+                <img
+                    src="./img/profile.jpeg"
+                    alt="profile"
+                    className="w-16 h-16 max-w-none rounded-full object-cover object-center block"
+                    style={{ aspectRatio: "1 / 1" }}
+                />
+                <h3 className="text-sm pt-1 text-center">Sauzan Faza</h3>
+            </div>
+
+            {/* Navigation Links */}
+            <nav className="flex flex-col items-center w-full">
+                <div className="w-full max-w-[150px] text-left space-y-2">
+                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-sm">
+                        <LuLayoutDashboard /> Dashboard
+                    </a>
+                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-sm">
+                        <FaShoppingCart /> Product
+                    </a>
+                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-sm">
+                        <FaUser /> Users
+                    </a>
+                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-sm">
+                        <BsStack /> Orders
+                    </a>
+                </div>
+            </nav>
+
+            {/* Logout Button - stays at bottom */}
+            <div className="mt-auto border-gray-700 pt-4">
+                <button className="flex items-center gap-2 w-full px-4 py-2 rounded-lg hover:bg-red-800 hover:text-white transition">
+                    <IoMdLogOut /> Logout
+                </button>
+            </div>
         </aside>
     );
 }
