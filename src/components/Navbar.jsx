a480736863f8fd6bf47432700bcca95e1a834e09
Sauzan Faza <sauzanfaza@gmail.com>
2025-09-20 14:41:46 +0700

    create a search bar component, but still without logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
+import { FaSearch } from "react-icons/fa";
+
 export default function Navbar() {
     return (
-        <header className="w-full bg-white shadow p-4 flex justify-between">
-            <h2 className="text-lg font-semibold">Dashboard</h2>
-            <p>Admin</p>
+        <header className="w-full bg-white shadow p-4 flex items-center justify-between">
+            <div className="flex items-center">
+            <h2 className="text-lg font-semibold">Welcome Sauzan Faza!</h2>
+            </div>
+            <div class="flex w-80">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="flex-1 border border-sky-800 rounded-l-full pl-4 py-2 focus:outline-none"
+                />
+                <button
+                    className="bg-gray-300 border border-l-0 border-sky-800 rounded-r-full px-4 flex items-center justify-center hover:text-sky-800"
+                >
+                    <FaSearch/>
+                </button>
+            </div>
         </header>
     );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,17 +20,17 @@ export default function Sidebar() {
 
             {/* Navigation Links */}
             <nav className="flex flex-col items-center w-full">
-                <div className="w-full max-w-[150px] text-left space-y-2">
-                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-sm">
+                <div className="w-full max-w-[150px] text-left space-y-1">
+                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-[12px]">
                         <LuLayoutDashboard /> Dashboard
                     </a>
-                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-sm">
+                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-[12px]">
                         <FaShoppingCart /> Product
                     </a>
-                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-sm">
+                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-[12px]">
                         <FaUser /> Users
                     </a>
-                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-sm">
+                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-[12px]">
                         <BsStack /> Orders
                     </a>
                 </div>
