d095276ee6c80eb1df6c192de43741af8f73dca4
Sauzan Faza <sauzanfaza@gmail.com>
2025-10-16 15:25:18 +0700

    add order list with status category filter in Orders page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ export default function Sidebar({user, setUser}) {
     }
 
     return (
-        <aside className="w-40 min-h-screen bg-gray-800 text-white p-4 flex flex-col">
+        <aside className="w-60 min-h-screen bg-gray-800 text-white p-4 flex flex-col">
             {/* Profile Section */}
             <div className="grid place-items-center mb-2 p-4">
                 <img
@@ -29,29 +29,29 @@ export default function Sidebar({user, setUser}) {
 
             {/* Navigation Links */}
             <nav className="flex flex-col items-center w-full">
-                <div className="w-full max-w-[150px] text-left space-y-1">
+                <div className="w-full max-w-[150px] text-left space-y-2">
                     <Link to="/mainBoard"
-                        className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-[12px]">
+                        className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-[20px]">
                         <LuLayoutDashboard /> Dashboard
                     </Link>
                     <Link to="/orders"
-                        className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-[12px]">
+                        className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-[20px]">
                         <FaShoppingCart /> Orders
                     </Link>
-                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-[12px]">
+                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-[20px]">
                         <FaUser /> Users
                     </a>
-                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-[12px]">
+                    <a href="#" className="flex items-center gap-2 hover:bg-sky-600 px-4 py-2 rounded-lg text-[20px]">
                         <BsStack /> Products
                     </a>
                 </div>
             </nav>
 
             {/* Logout Button*/}
-            <div className="mt-auto border-gray-700 pt-4">
+            <div className="mt-auto text-center border-gray-700 pt-4">
                 <button 
                     onClick={handleLogout}
-                    className="flex items-center gap-2 w-full px-4 py-2 rounded-lg hover:bg-red-800 hover:text-white transition">
+                    className="flex items-center justify-center gap-2 w-full px-4 py-2 rounded-lg hover:bg-red-800 hover:text-white transition">
                     <IoMdLogOut /> Logout
                 </button>
             </div>
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,15 +1,42 @@
 import Sidebar from "../components/Sidebar"
 import Navbar from "../components/Navbar"
 import { recentOrders } from "../data/data"
+import { useState } from "react"
 
 export default function Orders() {
+    const [selectedStatus, setSelectedStatus] = useState("All Orders")
+    
+    const filteredOrders = selectedStatus === "All Orders"
+        ? recentOrders 
+        : recentOrders.filter((order) => 
+            order.status.toLowerCase() === selectedStatus.toLowerCase())
+    
     return (
         <>
         <div className="flex">
             <Sidebar />
             <main className="flex-1 bg-gray-100">
                 <Navbar title="Orders"/>
-                <div className="bg-[#6D94C5] min-h-screen p-4">
+                {/*filter kategories  */}
+                <div className="grid grid-cols-2 bg-white px-8 py-4">
+                <div className="grid grid-cols-4 items-start">
+                {["All Orders", "Paid", "Pending", "Cancelled"].map((status) => (
+                        <button 
+                            key={status}
+                            onClick={() => setSelectedStatus(status)}
+                            className={`px-4 py-2font-medium transition ${
+                            selectedStatus === status
+                            ? " font-bold underline"
+                            : "hover:text-[#6D94C5]"
+                            }`}>
+                            {status}
+                        </button>
+                ))}
+                </div>
+                {/* <div></div> nnti di isi kok:)) */}
+                </div>
+                
+                <div className="bg-white min-h-screen py-4 px-8 text-sm">
                      {/*kategories*/}
                      <div className="grid grid-cols-5 bg-slate-200 rounded-lg">
                         <div className="p-2 text-center">Order ID</div>
@@ -18,17 +45,19 @@ export default function Orders() {
                         <div className="p-2 text-center">Date</div>
                         <div className="p-2 text-center">Status</div>
                      </div>
-                    {recentOrders.map((order) => (
-                        <div key={order.id} className="grid grid-cols-5 bg-white rounded-lg mt-4 py-2 shaodw-md">
+
+                     {/* data list */}
+                    {filteredOrders.map((order) => (
+                        <div key={order.id} className="grid grid-cols-5 bg-white border shadow-lg rounded-lg mt-4 py-2 shaodw-md hover:bg-[#6D94C5]">
                             <div className="p-2 text-center">{order.id}</div>
                             <div className="p-2 text-center">{order.customer}</div>
                             <div className="p-2 text-center">{order.address}</div>
                             <div className="p-2 text-center">{new Date(order.date).toLocaleDateString()}</div>
                             <div className={`p-2 text-center font-semibold ${
                                 order.status === "Paid"
-                                ? "text-green-600"
+                                ? "text-green-800"
                                 : order.status === "Pending"
-                                ? "text-yellow-600"
+                                ? "text-yellow-800"
                                 : "text-red-600"
                             }`}>{order.status}</div>
                         </div>
